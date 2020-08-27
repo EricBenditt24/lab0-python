987dc5919b11a5f6861f16fff8b0b5e642c7a3f4
EricBenditt <erb5623@psu.edu>
2020-08-27 21:56:03 +0000

    I moved steps around

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-print("Hello world!")
+
 name = input("Enter your name: ")
 print("hello, " + name + "!")  
 age = input ("Enter your age: ")
 age = int(age)
-print ("You will be " + str(age+1) + "next year")
-print (f"I am {age+1} next year")
+
+print ("You will be " + str(age+1) + " next year")
